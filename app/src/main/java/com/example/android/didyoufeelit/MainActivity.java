a48676d0161236593fb5716004a5a503804b63a7
Ivaylo Kotrabakov <ikotrabakov@gmail.com>
2016-09-13 14:43:44 +0300

    Handle empty or null cases in EarthquakeAsyncTask

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -58,6 +58,10 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected Event doInBackground(String... urls) {
+            // Don't perform the request if there are no URLs, or the first URL is null.
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
 
             // Perform the HTTP request for earthquake data and process the response.
             Event earthquake = Utils.fetchEarthquakeData(urls[0]);
@@ -66,6 +70,10 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(Event result) {
+            // If there is no result, do nothing.
+            if (result == null) {
+                return;
+            }
             updateUi(result);
         }
     }
